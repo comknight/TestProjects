986dbe5eb964221f29b3f637f95828afa962d80b
Robert Henry <dev.knight@hotmail.com>
2017-05-17 20:55:07 -0500

    Test Change

--- TestApp1/TestApp1/Program.cs
+++ TestApp1/TestApp1/Program.cs
@@ -6,7 +6,7 @@ namespace TestApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test App 1234");
         }
     }
 }
